e50465756e274ef48fee1958783da15308f5fb83
=luk1337 <priv.luk@gmail.com>
2025-01-29 13:03:57 +0500

    m21: Adjust override prop key names

--- lineage_m21.mk
+++ lineage_m21.mk
@@ -27,9 +27,9 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 # Build fingerprint
-BUILD_FINGERPRINT := "samsung/m21nsxx/m21:12/SP1A.210812.016/M215FXXU3CWB1:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="m21nsxx-user 12 SP1A.210812.016 M215FXXU3CWB1 release-keys"
+	BuildDesc="m21nsxx-user 12 SP1A.210812.016 M215FXXU3CWB1 release-keys" \
+	BuildFingerprint="samsung/m21nsxx/m21:12/SP1A.210812.016/M215FXXU3CWB1:user/release-keys"
 
 ## Device configuration
 PRODUCT_NAME := lineage_m21
